c7f2f631ce350d4f5895bc9a58f3639835962750
致爱蓝海 <1595711067@qq.com>
2025-11-30 17:42:51 +0800

    后端项目初始化

---
+++ src/main/java/com/lanhai/lanaicodemother/controller/HealthController.java
@@ -0,0 +1,17 @@
+package com.lanhai.lanaicodemother.controller;
+
+import com.lanhai.lanaicodemother.common.BaseResponse;
+import com.lanhai.lanaicodemother.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping("/")
+    public BaseResponse<String> healthCheck() {
+        return ResultUtils.success("ok");
+    }
+}
