a68c66f5787d5a9e4bc1393e4c6a45b817dfe8e6
致爱蓝海 <1595711067@qq.com>
2025-12-01 02:40:55 +0800

    后端用户模块开发-增删改查与权限控制->开发MyBatis Flex代码生成器、完成核心功能，比如用户注册、用户登录、获取登录用户信息、用户注销等、全局权限校验AOP、开发管理员增删改查接口、自定义配置解决丢失精度问题、备份sql语句

--- src/main/java/com/lanhai/lanaicodemother/LanAiCodeMotherApplication.java
+++ src/main/java/com/lanhai/lanaicodemother/LanAiCodeMotherApplication.java
@@ -1,11 +1,13 @@
 package com.lanhai.lanaicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.lanhai.lanaicodemother.mapper")
 public class LanAiCodeMotherApplication {
 
     public static void main(String[] args) {
